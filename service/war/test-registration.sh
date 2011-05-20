7da9c99ac0fbc59038d0c1de7739701d28eddf34
Charles Loomis <loomis@lal.in2p3.fr>
2011-05-20 16:57:18 +0200

    update service test

--- service/war/test-registration.sh
+++ service/war/test-registration.sh
@@ -1,10 +1,17 @@
 #!/bin/sh -x
 
-baseurl=http://localhost:8080
+baseurl=https://localhost:8444
+ldapurl=ldap://localhost:10389
 username=jsmith
 password=dummy_password
+new_password=dummy_password1
 
-CURL="curl --silent --show-error --fail"
+ldapmanager='uid=admin,ou=system'
+ldappasswd=secret
+
+CURL="curl --insecure --silent --show-error --fail"
+
+LDAPSEARCH="ldapsearch -LLL -H ${ldapurl} -x -D ${ldapmanager} -w ${ldappasswd}"
 
 #
 # Check that static pages exist.
@@ -56,4 +63,48 @@ if [ $rc != 0 ]; then
   exit 1
 fi
 
+#
+# Check that reset password works. 
+#
+reset_url=${baseurl}/reset/
+
+$CURL $reset_url \
+ --data "uid=${username}"
+
+rc=$?
+
+if [ $rc != 0 ]; then
+  echo "RESET ERROR: ${reset_url}"
+  exit 1
+fi
+
+#
+# Retrieve the action UUID.
+#
+uuid=`$LDAPSEARCH -b 'ou=actions,o=cloud' cn | grep cn: | cut -d ' ' -f 2`
+echo $uuid
+
+#
+# Click on the reset link.
+#
+#action_url=${baseurl}/actions/${uuid}/
+
+#$CURL $action_url
+
+#rc=$?
+
+#if [ $rc != 0 ]; then
+#  echo "ACTION URL ERROR: ${action_url}"
+#  exit 1
+#fi
+
+#
+# Check that LDAP user entry can be found. 
+#
+$LDAPSEARCH -b 'ou=users,o=cloud' '(objectClass=inetOrgPerson)'
+
+#
+# Check that LDAP group entry can be found. 
+#
+$LDAPSEARCH -b 'ou=groups,o=cloud' '(objectClass=groupOfUniqueNames)'
 
