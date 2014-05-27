9060162fce5effe91c7e2a34616a4d61ee577afc
Charles Loomis <cal@sixsq.com>
2014-05-27 18:12:33 +0200

    reformat code

--- service/war/src/main/java/eu/stratuslab/registration/main/RegistrationApplication.java
+++ service/war/src/main/java/eu/stratuslab/registration/main/RegistrationApplication.java
@@ -19,6 +19,20 @@
  */
 package eu.stratuslab.registration.main;
 
+import eu.stratuslab.registration.cfg.AppConfiguration;
+import eu.stratuslab.registration.cfg.Parameter;
+import eu.stratuslab.registration.guards.LdapVerifier;
+import eu.stratuslab.registration.resources.ActionResource;
+import eu.stratuslab.registration.resources.HomeResource;
+import eu.stratuslab.registration.resources.PoliciesResource;
+import eu.stratuslab.registration.resources.ProfileResource;
+import eu.stratuslab.registration.resources.ProfileUpdatedResource;
+import eu.stratuslab.registration.resources.RegisterResource;
+import eu.stratuslab.registration.resources.ResetResource;
+import eu.stratuslab.registration.resources.ResetStartedResource;
+import eu.stratuslab.registration.resources.SuccessResource;
+import eu.stratuslab.registration.resources.UsersResource;
+import eu.stratuslab.registration.utils.RequestUtils;
 import org.restlet.Application;
 import org.restlet.Context;
 import org.restlet.Request;
@@ -34,140 +48,123 @@ import org.restlet.routing.Template;
 import org.restlet.routing.TemplateRoute;
 import org.restlet.security.ChallengeAuthenticator;
 
-import eu.stratuslab.registration.cfg.AppConfiguration;
-import eu.stratuslab.registration.cfg.Parameter;
-import eu.stratuslab.registration.guards.LdapVerifier;
-import eu.stratuslab.registration.resources.ActionResource;
-import eu.stratuslab.registration.resources.HomeResource;
-import eu.stratuslab.registration.resources.PoliciesResource;
-import eu.stratuslab.registration.resources.ProfileResource;
-import eu.stratuslab.registration.resources.ProfileUpdatedResource;
-import eu.stratuslab.registration.resources.RegisterResource;
-import eu.stratuslab.registration.resources.ResetResource;
-import eu.stratuslab.registration.resources.ResetStartedResource;
-import eu.stratuslab.registration.resources.SuccessResource;
-import eu.stratuslab.registration.resources.UsersResource;
-import eu.stratuslab.registration.utils.RequestUtils;
-
 public class RegistrationApplication extends Application {
 
-	public RegistrationApplication() {
+    public RegistrationApplication() {
 
-		super();
+        super();
 
-		setName("StratusLab Registration Application");
-		setDescription("StratusLab server to allow web-based registration.");
-		setOwner("StratusLab");
-		setAuthor("Charles Loomis");
+        setName("StratusLab Registration Application");
+        setDescription("StratusLab server to allow web-based registration.");
+        setOwner("StratusLab");
+        setAuthor("Charles Loomis");
 
-		getTunnelService().setUserAgentTunnel(true);
+        getTunnelService().setUserAgentTunnel(true);
 
-		setStatusService(new CommonStatusService());
+        setStatusService(new CommonStatusService());
 
-	}
+    }
 
-	@Override
-	public Restlet createInboundRoot() {
+    @Override
+    public Restlet createInboundRoot() {
 
-		// Do not do this during the constructor because the context will not
-		// yet have been initialized.
-		AppConfiguration appConfiguration = new AppConfiguration(getContext());
+        // Do not do this during the constructor because the context will not
+        // yet have been initialized.
+        AppConfiguration appConfiguration = new AppConfiguration(getContext());
 
-		Router router = new RootRouter(getContext(), appConfiguration);
+        Router router = new RootRouter(getContext(), appConfiguration);
 
-		TemplateRoute route = null;
+        TemplateRoute route = null;
 
         route = router.attach("/users/", UsersResource.class);
-		router.attach("/users", UsersResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/users", UsersResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/register/", RegisterResource.class);
-		router.attach("/register", RegisterResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/register/", RegisterResource.class);
+        router.attach("/register", RegisterResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/success/", SuccessResource.class);
-		router.attach("/success", SuccessResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/success/", SuccessResource.class);
+        router.attach("/success", SuccessResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/profile/", setupGuard(ProfileResource.class));
-		router.attach("/profile", setupGuard(ProfileResource.class));
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/profile/", setupGuard(ProfileResource.class));
+        router.attach("/profile", setupGuard(ProfileResource.class));
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/profile_updated/", ProfileUpdatedResource.class);
-		router.attach("/profile_updated", ProfileUpdatedResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/profile_updated/", ProfileUpdatedResource.class);
+        router.attach("/profile_updated", ProfileUpdatedResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/policies/", PoliciesResource.class);
-		router.attach("/policies", PoliciesResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/policies/", PoliciesResource.class);
+        router.attach("/policies", PoliciesResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/reset/", ResetResource.class);
-		router.attach("/reset", ResetResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/reset/", ResetResource.class);
+        router.attach("/reset", ResetResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/reset_started/", ResetStartedResource.class);
-		router.attach("/reset_started", ResetStartedResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/reset_started/", ResetStartedResource.class);
+        router.attach("/reset_started", ResetStartedResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/action/", ActionResource.class);
-		router.attach("/action", ActionResource.class);
-		route.setMatchingMode(Template.MODE_EQUALS);
+        router.attach("/action/", ActionResource.class);
+        router.attach("/action", ActionResource.class);
+        route.setMatchingMode(Template.MODE_EQUALS);
 
-		router.attach("/action/{uuid}", ActionResource.class);
+        router.attach("/action/{uuid}", ActionResource.class);
 
-		Reference styleRef = LocalReference.createClapReference(
-				LocalReference.CLAP_THREAD,
-				appConfiguration.getValue(Parameter.STYLE_PATH));
+        Reference styleRef = LocalReference
+                .createClapReference(LocalReference.CLAP_THREAD, appConfiguration.getValue(Parameter.STYLE_PATH));
 
-		Directory cssDir = new Directory(getContext(), styleRef);
-		cssDir.setNegotiatingContent(false);
-		cssDir.setIndexName("index.html");
-		router.attach("/css/", cssDir);
-		router.attach("/css", cssDir);
+        Directory cssDir = new Directory(getContext(), styleRef);
+        cssDir.setNegotiatingContent(false);
+        cssDir.setIndexName("index.html");
+        router.attach("/css/", cssDir);
+        router.attach("/css", cssDir);
 
-		router.attach("/media/", createMediaDirectory(getContext()));
+        router.attach("/media/", createMediaDirectory(getContext()));
 
-		router.attachDefault(HomeResource.class);
+        router.attachDefault(HomeResource.class);
 
-		return router;
-	}
+        return router;
+    }
 
-	private static Directory createMediaDirectory(Context context) {
+    private static Directory createMediaDirectory(Context context) {
 
-		Directory mediaDir = new Directory(context, "war:///media");
-		mediaDir.setNegotiatingContent(false);
-		mediaDir.setIndexName("index.html");
+        Directory mediaDir = new Directory(context, "war:///media");
+        mediaDir.setNegotiatingContent(false);
+        mediaDir.setIndexName("index.html");
 
-		return mediaDir;
-	}
+        return mediaDir;
+    }
 
-	public ChallengeAuthenticator setupGuard(Class<? extends ServerResource> targetClass) {
+    public ChallengeAuthenticator setupGuard(Class<? extends ServerResource> targetClass) {
 
-		ChallengeAuthenticator guard = new ChallengeAuthenticator(null,
-				ChallengeScheme.HTTP_BASIC, "StratusLab");
+        ChallengeAuthenticator guard = new ChallengeAuthenticator(null, ChallengeScheme.HTTP_BASIC, "StratusLab");
 
-		guard.setVerifier(new LdapVerifier());
+        guard.setVerifier(new LdapVerifier());
 
-		guard.setNext(targetClass);
+        guard.setNext(targetClass);
 
-		return guard;
-	}
+        return guard;
+    }
 
-	public static class RootRouter extends Router {
+    public static class RootRouter extends Router {
 
-		private final AppConfiguration appConfiguration;
+        private final AppConfiguration appConfiguration;
 
-		public RootRouter(Context context, AppConfiguration appConfiguration) {
-			super(context);
-			this.appConfiguration = appConfiguration;
-		}
+        public RootRouter(Context context, AppConfiguration appConfiguration) {
+            super(context);
+            this.appConfiguration = appConfiguration;
+        }
 
-		@Override
-		public void doHandle(Restlet next, Request request, Response response) {
-			RequestUtils.insertAppConfiguration(request, appConfiguration);
-			super.doHandle(next, request, response);
-		}
+        @Override
+        public void doHandle(Restlet next, Request request, Response response) {
+            RequestUtils.insertAppConfiguration(request, appConfiguration);
+            super.doHandle(next, request, response);
+        }
 
-	}
+    }
 
 }
